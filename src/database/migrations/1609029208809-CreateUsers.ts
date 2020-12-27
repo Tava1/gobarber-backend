3e4234ccadc04593b19dcd7cc57b56071be5671c
Gustavo Santos <gusta99sant@gmail.com>
2020-12-27 19:21:09 -0300

    Change type of the id field to uuid

--- src/database/migrations/1609029208809-CreateUsers.ts
+++ src/database/migrations/1609029208809-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1609029208809 implements MigrationInterface {
         columns: [
           {
             name: 'id',
-            type: 'varchar',
+            type: 'uuid',
             isPrimary: true,
             generationStrategy: 'uuid',
             default: 'uuid_generate_v4()',
