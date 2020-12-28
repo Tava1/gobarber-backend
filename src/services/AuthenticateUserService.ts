c896ddb4ba0cea42f308a33c2ea6344d2b17384c
Gustavo Santos <gusta99sant@gmail.com>
2020-12-28 18:43:20 -0300

    Generate JWT Token

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -33,8 +35,15 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
+    // {}: payload, 'etc' chave secreta e {}
+    const token = sign({}, '7391d43a4dee168cf0d5e57353fc434b', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
+
     return {
       user,
+      token,
     };
   }
 }
