035d72476aabc46be97a6755362010a008838416
Gustavo Santos <gusta99sant@gmail.com>
2020-12-30 16:07:26 -0300

    New TS version to delete password property

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -21,9 +21,17 @@ usersRouter.post('/', async (request, response) => {
       password,
     });
 
-    delete user.password;
+    // delete user.password;
+    // Nova versão do TS
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
 
-    return response.json(user);
+    return response.json(userWithoutPassword);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
